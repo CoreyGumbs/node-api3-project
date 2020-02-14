587ef39e1f67f97c96e8be1bfdf305e8ae195053
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-13 22:40:57 -0500

    [Add] All endpoints except user post endpoint completed

--- middleware/validateUserId.js
+++ middleware/validateUserId.js
@@ -3,18 +3,18 @@ const user_db = require('../users/userDb');
 function validateUserId(req, res, next){
     user_db.getById(req.params.id)
     .then(user => {
+       
         if(req.params.id != user.id){
             res.status(400).json({message: "invalid user id"});
         }else{
-            req.id = user.id;
+            req.user = user;
             res.status(200).json(user);
+            next();
         }   
     })
     .catch(error => {
         res.status(500).json({error: "There was trouble retrieving user."})
-    })
-
-    next();
+    });
 
 }
 
--- users/userRouter.js
+++ users/userRouter.js
@@ -5,6 +5,7 @@ const validateUserId = require('../middleware/validateUserId');
 
 const router = express.Router();
 
+//creates user
 router.post('/', validateUser, (req, res) => {
    user_db.insert(req.body)
    .then(user => {
@@ -19,24 +20,25 @@ router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
+//gets all users
 router.get('/', (req, res) => {
   user_db.get()
   .then(user => {
     res.status(200).json(user);
   })
   .catch(error => {
-    res.status(500).json({error: "There was trouble retrieving Users"});
+    res.status(500).json({error: "There was trouble retrieving users."});
   })
 });
 
+//gets user by id
 router.get('/:id', validateUserId, (req, res) => {
-  const {id} = req.params;
-
+  const {id} = req.user;
+ 
   user_db.getById(id)
   .then(user => {
 
     res.status(200).json(user);
-
   })
   .catch(error => {
 
@@ -45,12 +47,14 @@ router.get('/:id', validateUserId, (req, res) => {
   });
 });
 
-router.get('/:id/posts', (req, res) => {
-  const {id} = req.params;
+//gets user posts by id
+router.get('/:id/posts', validateUserId, (req, res) => {
+  const {id} = req.user;
 
   user_db.getUserPosts(id)
   .then(posts => {
     res.status(200).json(posts);
+    console.log(req.user);
   })
   .catch(error => {
     res.status(500).json({error: 'There was an issue retrieveing user posts'});
@@ -58,20 +62,34 @@ router.get('/:id/posts', (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+//deletes user by id
+router.delete('/:id', validateUserId, (req, res) => {
+  const {id} = req.user;
+
+  user_db.remove(id)
+  .then(user => {
+    res.status(200).json({message: "user deleted."});
+  }) 
+  .catch(error => {
+    res.status(500).json({error: "There was a problem. User not deleted."});
+  })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+//updates user by id
+router.put('/:id', validateUser, validateUserId, (req, res) => {
+  const { id } = req.user;
+  const { body } = req;
+  
+  user_db.update(id, body )
+  .then(user => {
+    res.status(200);
+  })
+  .catch(error => {
+    res.status(500).json({error: "There was a problem with updating."});
+  })
 });
 
 //custom middleware
-
-// function validateUserId(req, res, next) {
-//   // do your magic!
-// }
-
 
 // function validatePost(req, res, next) {
 //   // do your magic!
